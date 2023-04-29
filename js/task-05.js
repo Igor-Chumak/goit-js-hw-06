2c21550072b2bb1617f5630083bb1be39d251ce0
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-04-29 11:24:11 +0300

    5

--- js/task-05.js
+++ js/task-05.js
@@ -2,3 +2,10 @@
 
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+inputName() => {
+    
+};
+
+const input = document.querySelector("#name-input");
+
+input.addEventListener("input", inputName);
