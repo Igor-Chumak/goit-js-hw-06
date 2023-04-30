a840cd6d6c099716922b782de3f7e3e1e818cd0f
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-04-30 15:51:10 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -12,6 +12,11 @@
 
 // 1. Размеры самого первого <div> - 30px на 30px.
 // 2. Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+
+function createBoxes(amount) {
+  console.log("Create:", amount, " boxes");
+}
+
 // 3. Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
 
 function getRandomHexColor() {
@@ -22,5 +27,21 @@ function getRandomHexColor() {
 
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
 
+function destroyBoxes() {
+  console.log("Destroy");
+}
+//
+
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+btnCreate.addEventListener("click", (event) => {
+  console.log("Value:", event.currentTarget);
+  createBoxes(event.currentTarget.value);
+});
+
+btnDestroy.addEventListener("click", destroyBoxes);
+
 // console.log(`${formRef} --- ${formRef}`);
-// console.log("formRef", ": ", formRef);
+// console.log("btnCreate", ": ", btnCreate);
+// console.log("btnDestroy", ": ", btnDestroy);
